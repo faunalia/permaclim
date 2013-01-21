c51e5d09f0b0b170f3d20c8fdfbe6e9a00b9e501
Riccardo Lemmi <riccardo@reflab.com>
2013-01-21 14:29:35 +0100

    copy models in the proper directory

--- SextantePermaclimProviderPlugin.py
+++ SextantePermaclimProviderPlugin.py
@@ -26,20 +26,46 @@ __copyright__ = '(C) 2013, Riccardo Lemmi'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
 
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+
 from qgis.core import *
-import os, sys
+import os, sys, shutil
 import inspect
+
 from sextante.core.Sextante import Sextante
+from sextante.modeler.ModelerUtils import ModelerUtils
+
+from permaclim import version
 from permaclim.PermaclimAlgorithmProvider import PermaclimAlgorithmProvider
+
 
 cmd_folder = os.path.split(inspect.getfile( inspect.currentframe() ))[0]
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
+
 class SextantePermaclimProviderPlugin:
 
     def __init__(self):
         self.provider = PermaclimAlgorithmProvider()
+
+        settings = QSettings()
+        version_settings = settings.value( "/version", '').toString()
+        current_version = version()
+
+        if version_settings != current_version:
+            settings.setValue("/version", current_version)
+
+            models_src_path = os.path.join(cmd_folder, 'models')
+            models_dst_path = ModelerUtils.modelsFolder()
+
+            for name in os.listdir(models_src_path):
+                file_src_path = os.path.join(models_src_path, name)
+                file_dst_path = os.path.join(models_dst_path, name)
+                if os.path.exists(file_dst_path):
+                    shutil.move(file_dst_path, file_dst_path+'.old')
+                shutil.copy(file_src_path, file_dst_path)
 
     def initGui(self):
         Sextante.addProvider(self.provider)
